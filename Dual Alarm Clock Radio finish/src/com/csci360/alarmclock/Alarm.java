814d1858aabae75ca0f75fc67e04efe1bc005ec4
mkay0961 <mkay0961@gmail.com>
2017-04-11 12:17:51 -0400

    Add files via upload

---
+++ Dual Alarm Clock Radio finish/src/com/csci360/alarmclock/Alarm.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.csci360.alarmclock;
+
+/**
+ *
+ * @author MatthewKay
+ */
+public class Alarm {
+    	private int alarmNum;
+        private int hours;
+        private int minutes;
+        private int seconds;
+        private boolean isPM;
+        private boolean isAlarmON;
+        private boolean isGoingOff;
+
+public Alarm(int alarmNum, int hours, int minutes, boolean isPm, boolean isAlarmON) {
+    this.alarmNum = alarmNum;
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = 0;
+    this.isPM = isPm;
+    this.isAlarmON  = isAlarmON;
+    }
+
+             	
+public boolean getIsPM() {
+    return isPM;
+ }          
+     	
+public int getAlarmNum(){
+    return alarmNum;
+ }
+              	
+public boolean getIsAlarmON() {
+    return isAlarmON;
+}
+
+public int getHours(){
+    return hours;
+ }
+ 
+public int getMinutes(){
+    return  minutes;
+}
+
+public void setIsAlarmON(boolean AlarmON) throws NullPointerException{
+    this.isAlarmON = AlarmON;
+}
+
+public void setHours(int hours){
+    this.hours = hours;
+}
+
+public void setMinutes(int minutes){
+    this.minutes = minutes;
+}
+ 
+public void setIsPM(boolean isPM) {
+    this.isPM = isPM;
+}
+
+public boolean isIsGoingOff() {
+        return isGoingOff;
+    }
+
+public void setIsGoingOff(boolean isGoingOff) {
+        this.isGoingOff = isGoingOff;
+    }
+
+public int getSeconds() {
+        return seconds;
+    }
+ 
+public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+public String toString(){
+    String rtn = "";
+    rtn += this.hours + " : " ;
+    if(this.minutes < 10){
+        rtn +="0" + this.minutes;
+        }
+        else{
+            rtn +=this.minutes;
+        }
+    
+    
+    
+    
+    if(isPM){
+            rtn+= " PM";
+        }
+        else{
+            rtn+= " AM";
+        }
+    return rtn;
+    
+}
+
+
+
+
+
+
+ 
+              	
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
---
+++ Dual Alarm Clock Radio finish/src/com/csci360/alarmclock/Radio.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.csci360.alarmclock;
+
+/**
+ *
+ * @author MatthewKay
+ */
+public class Radio {
+
+    private double stationNumFM;
+    private int stationNumAM;
+    private boolean isAM;
+    private boolean isON;
+    private int volume;
+    
+    public Radio() {
+        
+        stationNumFM = 99.5;
+        stationNumAM = 730;
+        isAM = false;
+        isON = false;
+        volume = 0;
+}
+    
+    public void setStationNumFM(double x){
+        stationNumFM = x;
+    }
+    
+    public void setStationNumAM(int x){
+        stationNumAM = x;
+    }
+    
+    public void setradioSetting(boolean x){
+        isAM = x;
+    }
+    
+    public double getStationNumFM(){
+       return stationNumFM;
+    }
+    
+    public int getStationNumAM(){
+       return stationNumAM;
+    }
+    
+    public boolean getRadioSetting(){
+       return isAM;
+    }
+
+    public boolean isIsON() {
+        return isON;
+    }
+
+    public void setIsON(boolean isON) {
+        this.isON = isON;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    
+    public String toString(){
+        String rtn = "";
+        if(isAM == true){
+            rtn += "AM: ";
+            rtn += stationNumAM;
+        }
+        else{
+            rtn += "FM: ";
+            rtn += stationNumFM;
+        }
+        return rtn;
+        
+       
+                
+        
+    }
+    
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+}
